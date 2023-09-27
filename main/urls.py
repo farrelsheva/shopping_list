358c95021afe3125dabd2398a836f72e73fe99db
farrelsheva <farrelshevaa@gmail.com>
2023-09-27 13:24:15 +0700

    added delete and edit

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, \
-    show_json,show_xml_by_id, show_json_by_id, \
+from main.views import show_main, create_product,edit_product,delete_product, \
+    show_xml, show_json,show_xml_by_id, show_json_by_id, \
     register, login_user, logout_user
 
 app_name = 'main'
@@ -8,10 +8,14 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
+    path('edit_product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'), # sesuaikan dengan nama fungsi ya,
+   
     path('xml/',show_xml,name='show_xml'),
     path('json/',show_json,name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+   
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout')
